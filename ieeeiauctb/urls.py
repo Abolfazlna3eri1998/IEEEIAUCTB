30e9d87250f8bfba85a48b027ecb78f3460b2c21
abolfazlnaseri <abolfazl.na3eri18@gmail.com>
2019-11-08 11:26:24 +0330

    test

---
+++ ieeeiauctb/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.index),
+    path('index.html',views.homepage),
+    path('registerform.html',views.register),
+    path('tamasbama.html',views.contactus)
+]
---
+++ ieeeiauctb/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, request
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "base.html", {})
+def homepage(request):
+    return render(request,"index.html")
+def register(request):
+    return render(request,"registerform.html")
+def contactus(request):
+    return render(request,"tamasbama.html")
+
