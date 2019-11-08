0966bb5cb6225c64a9279e8b19c0c8f31127a663
abolfazlnaseri <abolfazl.na3eri18@gmail.com>
2019-11-08 15:06:53 +0330

    test5

--- ieeeiauctb/urls.py
+++ ieeeiauctb/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('',views.index, name='base'),
     path('index.html',views.homepage, name='index'),
-    path('registerform.html',views.register,name=register),
-    path('tamasbama.html',views.contactus,name=contactus),
-    path('login.html',views.login,name=login)
+    path('registerform.html',views.register,name='register'),
+    path('tamasbama.html',views.contactus,name='contactus'),
+    #path('login.html',views.login,name='login')
 ]
--- ieeeiauctb/views.py
+++ ieeeiauctb/views.py
@@ -10,4 +10,6 @@ def register(request):
     return render(request,"registerform.html")
 def contactus(request):
     return render(request,"tamasbama.html")
+def login(request) :
+    return render(request,"login.html")
 
