4351f1563e9c8236eba0bc10c84c908dfbf4ced6
abolfazlnaseri <abolfazl.na3eri18@gmail.com>
2019-11-08 14:49:46 +0330

    test5

--- ieeeiauctb/urls.py
+++ ieeeiauctb/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path
 
+from ieeeiauctb.views import contactus, register
 from . import views
 
 
 urlpatterns = [
-    path('',views.index),
-    path('index.html',views.homepage),
-    path('registerform.html',views.register),
-    path('tamasbama.html',views.contactus),
+    path('',views.index, name='base'),
+    path('index.html',views.homepage, name='index'),
+    path('registerform.html',views.register,name=register),
+    path('tamasbama.html',views.contactus,name=contactus),
+    path('login.html',views.login,name=login)
 ]
