e413860c61ce5dea748a05d9c59944b7a2e5e706
abolfazlnaseri <abolfazl.na3eri18@gmail.com>
2019-11-08 15:20:42 +0330

    as

--- ieeeiauctb/urls.py
+++ ieeeiauctb/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('',views.index, name='base'),
     path('index.html',views.homepage, name='index'),
     path('registerform.html',views.register,name='register'),
-    path('tamasbama.html',views.contactus,name='contactus'),
+    path('tamasbama.html',views.contactus,name='contact'),
     #path('login.html',views.login,name='login')
 ]
--- ieeeiauctb/views.py
+++ ieeeiauctb/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html", {})
-def homepage(request):
-    return render(request,"index.html")
-def register(request):
-    return render(request,"registerform.html")
-def contactus(request):
-    return render(request,"tamasbama.html")
 
+
+def homepage(request):
+    return render(request, "index.html")
+
+
+def register(request):
+    return render(request, "registerform.html")
+
+
+def contactus(request):
+    return render(request, "tamasbama.html")
